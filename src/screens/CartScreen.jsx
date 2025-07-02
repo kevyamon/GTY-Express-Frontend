100a3f42941c8bc1d50ce00637e23e53fd1f21bb
kevyamon <gamekevy@gmail.com>
2025-07-02 20:33:32 +0000

    Update CartScreen.jsx

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
 import Message from '../components/Message';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
-  // Pour l'instant, le panier est vide. Nous ajouterons la logique plus tard.
-  const cartItems = []; 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
 
   return (
     <Row>
@@ -17,9 +33,65 @@ const CartScreen = () => {
           </Message>
         ) : (
           <ListGroup variant='flush'>
-            {/* La liste des produits du panier s'affichera ici */}
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>{item.price} €</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) => dispatch(addToCart({...item, qty: Number(e.target.value)}))}
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
+                      🗑️
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Sous-total ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                articles
+              </h2>
+              {cart.itemsPrice} €
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+              >
+                Passer la commande
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   );
