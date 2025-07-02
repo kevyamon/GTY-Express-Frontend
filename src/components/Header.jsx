10d222bd35be12878a35848e0b49fdc35d19c3b4
kevyamon <gamekevy@gmail.com>
2025-07-02 18:17:42 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,53 +6,37 @@ import { logout } from '../slices/authSlice';
 
 const Header = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const [logoutApiCall] = useLogoutMutation();
-
-  const logoutHandler = async () => {
-    try {
-      await logoutApiCall().unwrap();
-      dispatch(logout());
-      navigate('/login');
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
+  // ... reste du code existant ...
+  
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand as={Link} to="/">
-            GTY Express
-          </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">GTY Express</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               {userInfo ? (
                 <>
-                  <Nav.Link as={Link} to="/cart">
-                    🛒 Panier
-                  </Nav.Link>
+                  <Nav.Link as={Link} to="/cart">🛒 Panier</Nav.Link>
                   <NavDropdown title={userInfo.name} id='username'>
-                    <Nav.Link as={Link} to="/products" className="dropdown-item">
-                      Produits
-                    </Nav.Link>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Déconnexion
-                    </NavDropdown.Item>
+                    <Nav.Link as={Link} to="/products" className="dropdown-item">Produits</Nav.Link>
+                    {/* LIENS ADMIN */}
+                    {userInfo && userInfo.isAdmin && (
+                      <>
+                        <NavDropdown.Divider />
+                        <Nav.Link as={Link} to='/admin/productlist' className="dropdown-item">Gestion Produits</Nav.Link>
+                        {/* Ajoutez ici les futurs liens (Gestion Commandes, etc.) */}
+                      </>
+                    )}
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item onClick={logoutHandler}>Déconnexion</NavDropdown.Item>
                   </NavDropdown>
                 </>
               ) : (
                 <>
-                  <Nav.Link as={Link} to="/login">
-                    Se Connecter
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/register">
-                    S'inscrire
-                  </Nav.Link>
+                  <Nav.Link as={Link} to="/login">Se Connecter</Nav.Link>
+                  <Nav.Link as={Link} to="/register">S'inscrire</Nav.Link>
                 </>
               )}
             </Nav>
