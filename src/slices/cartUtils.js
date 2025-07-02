b567bf5cd050d41d4ceed15fa13343432ee1359a
kevyamon <gamekevy@gmail.com>
2025-07-02 22:20:49 +0000

    Delete src/slices/cartUtils.js

--- src/slices/cartUtils.js
+++
@@ -1,9 +0,0 @@
-export const updateCart = (state) => {
-  // Calculer le prix des articles
-  state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
-
-  // Sauvegarder le panier dans le localStorage
-  localStorage.setItem('cart', JSON.stringify(state));
-
-  return state;
-};
