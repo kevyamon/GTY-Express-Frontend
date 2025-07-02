354309044c7baecd953ba67c64029842c903f751
kevyamon <gamekevy@gmail.com>
2025-07-02 20:26:51 +0000

    Update ProductScreen.jsx

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,14 +1,30 @@
-import { useParams, Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice.js';
+import { addToCart } from '../slices/cartSlice';
+import { toast } from 'react-toastify';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [qty, setQty] = useState(1);
+
   const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    toast.success('Produit ajouté au panier !');
+    // Optionnel : rediriger vers le panier
+    // navigate('/cart');
+  };
 
   return (
     <>
-      <Link className="btn btn-light my-3" to="/">
+      <Link className="btn btn-light my-3" to="/products">
         Retour
       </Link>
       {isLoading ? (
@@ -25,7 +41,7 @@ const ProductScreen = () => {
               <ListGroup.Item>
                 <h3>{product.name}</h3>
               </ListGroup.Item>
-              <ListGroup.Item>Prix: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>Prix: {product.price} €</ListGroup.Item>
               <ListGroup.Item>Description: {product.description}</ListGroup.Item>
             </ListGroup>
           </Col>
@@ -35,7 +51,7 @@ const ProductScreen = () => {
                 <ListGroup.Item>
                   <Row>
                     <Col>Prix:</Col>
-                    <Col><strong>${product.price}</strong></Col>
+                    <Col><strong>{product.price} €</strong></Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -44,11 +60,32 @@ const ProductScreen = () => {
                     <Col>{product.countInStock > 0 ? 'En Stock' : 'Rupture de stock'}</Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qté</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}
+                    onClick={addToCartHandler}
                   >
                     Ajouter au Panier
                   </Button>
@@ -61,4 +98,5 @@ const ProductScreen = () => {
     </>
   );
 };
+
 export default ProductScreen;
