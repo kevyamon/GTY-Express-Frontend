fe9276452e032ecf8367c1b3a099cec5c3af2b49
kevyamon <gamekevy@gmail.com>
2025-07-03 23:39:49 +0000

    Améliorations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,83 @@
-// ... dans Header.jsx ...
-<NavDropdown title={userInfo.name} id='username'>
-  <NavDropdown.Item as={Link} to="/products">Produits</NavDropdown.Item>
-  
-  {/* NOUVEAU LIEN */}
-  <NavDropdown.Item as={Link} to="/favorites">Mes Favoris</NavDropdown.Item>
-  
-  {userInfo && userInfo.isAdmin && (
-    // ...
-  )}
-  <NavDropdown.Divider />
-  <NavDropdown.Item onClick={logoutHandler}>
-    Déconnexion
-  </NavDropdown.Item>
-</NavDropdown>
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+
+const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            GTY Express
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/cart">
+                🛒 Panier
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+
+              {userInfo ? (
+                // Vue quand l'utilisateur est connecté
+                <NavDropdown title={userInfo.name} id="username">
+                  <NavDropdown.Item as={Link} to="/products">
+                    Produits
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/favorites">
+                    Mes Favoris
+                  </NavDropdown.Item>
+
+                  {userInfo.isAdmin && (
+                    <>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item as={Link} to="/admin/productlist">
+                        Gestion Produits
+                      </NavDropdown.Item>
+                    </>
+                  )}
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Déconnexion
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                // Vue quand l'utilisateur n'est pas connecté
+                <Nav.Link as={Link} to="/login">
+                  👤 Se Connecter
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
