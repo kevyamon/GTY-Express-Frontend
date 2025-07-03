019350c84ff363d58f78daeaba7a5c709ebc6611
kevyamon <gamekevy@gmail.com>
2025-07-03 16:42:31 +0000

    Améliorations du fond

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css'; // On importe le style pour l'animation
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <main className="py-3">
-        {/* On ajoute "fluid" ici pour élargir la zone de contenu */}
-        <Container fluid> 
-          <Outlet />
-        </Container>
+        <TransitionGroup component={null}>
+          <CSSTransition key={location.key} timeout={300} classNames="fade">
+            <Container fluid>
+              <Outlet />
+            </Container>
+          </CSSTransition>
+        </TransitionGroup>
       </main>
       <Footer />
       <ToastContainer />
