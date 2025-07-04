75d6de5191edfe31f74a9cb08dea10614d2d7374
kevyamon <gamekevy@gmail.com>
2025-07-04 04:30:22 +0000

    Am

--- src/screens/PlaceOrderScreen.jsx
+++ src/screens/PlaceOrderScreen.jsx
@@ -10,7 +10,10 @@ import { clearCartItems } from '../slices/cartSlice';
 
 const PlaceOrderScreen = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const cart = useSelector((state) => state.cart);
+
   const [createOrder, { isLoading, error }] = useCreateOrderMutation();
 
   useEffect(() => {
@@ -21,7 +24,6 @@ const PlaceOrderScreen = () => {
     }
   }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
 
-  const dispatch = useDispatch();
   const placeOrderHandler = async () => {
     try {
       const res = await createOrder({
@@ -36,41 +38,110 @@ const PlaceOrderScreen = () => {
       dispatch(clearCartItems());
       navigate(`/order/${res._id}`);
     } catch (err) {
-      toast.error(err);
+      toast.error(err?.data?.message || err.error);
     }
   };
 
   return (
     <>
-      <CheckoutSteps step1 step2 step3 />
+      <CheckoutSteps step={3} />
       <Row>
         <Col md={8}>
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>Livraison</h2>
-              <p><strong>Adresse:</strong> {cart.shippingAddress.address}, {cart.shippingAddress.city} {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}</p>
+              <p>
+                <strong>Adresse: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{' '}
+                {cart.shippingAddress.country}
+              </p>
             </ListGroup.Item>
+
             <ListGroup.Item>
               <h2>Paiement</h2>
-              <strong>Méthode: </strong> {cart.paymentMethod}
+              <p>
+                <strong>Méthode: </strong>
+                {cart.paymentMethod}
+              </p>
             </ListGroup.Item>
+
             <ListGroup.Item>
               <h2>Articles</h2>
-              {cart.cartItems.length === 0 ? ( <Message>Votre panier est vide</Message> ) 
-              : ( <ListGroup variant='flush'> {cart.cartItems.map((item, index) => ( <ListGroup.Item key={index}> <Row> <Col md={1}> <Image src={item.image} alt={item.name} fluid rounded /> </Col> <Col> <Link to={`/product/${item.product}`}>{item.name}</Link> </Col> <Col md={4}> {item.qty} x {item.price} FCFA = {item.qty * item.price} FCFA </Col> </Row> </ListGroup.Item> ))} </ListGroup> </ListGroup.Item>
+              {cart.cartItems.length === 0 ? (
+                <Message>Votre panier est vide</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image.startsWith('/') ? `${import.meta.env.VITE_BACKEND_URL}${item.image}` : item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item._id}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x {item.price} FCFA = {(item.qty * item.price).toFixed(2)} FCFA
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={4}>
           <Card>
             <ListGroup variant='flush'>
-              <ListGroup.Item><h2>Récapitulatif</h2></ListGroup.Item>
-              <ListGroup.Item><Row><Col>Articles</Col><Col>{cart.itemsPrice.toFixed(2)} FCFA</Col></Row></ListGroup.Item>
-              <ListGroup.Item><Row><Col>Frais de port</Col><Col>{cart.shippingPrice.toFixed(2)} FCFA</Col></Row></ListGroup.Item>
-              <ListGroup.Item><Row><Col>Taxes</Col><Col>{cart.taxPrice.toFixed(2)} FCFA</Col></Row></ListGroup.Item>
-              <ListGroup.Item><Row><Col>Total</Col><Col>{cart.totalPrice.toFixed(2)} FCFA</Col></Row></ListGroup.Item>
-              <ListGroup.Item>{error && ( <Message variant='danger'>{error.data.message}</Message> )} </ListGroup.Item>
               <ListGroup.Item>
-                <Button type='button' className='btn-block' disabled={cart.cartItems === 0} onClick={placeOrderHandler}>Valider la Commande</Button>
+                <h2>Récapitulatif</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Articles</Col>
+                  <Col>{cart.itemsPrice.toFixed(2)} FCFA</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Frais de port</Col>
+                  <Col>{cart.shippingPrice.toFixed(2)} FCFA</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Taxes</Col>
+                  <Col>{cart.taxPrice.toFixed(2)} FCFA</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>{cart.totalPrice.toFixed(2)} FCFA</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && (
+                  <Message variant='danger'>{error.data.message}</Message>
+                )}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type='button'
+                  className='btn-block w-100'
+                  disabled={cart.cartItems.length === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Valider la Commande
+                </Button>
                 {isLoading && <p>Chargement...</p>}
               </ListGroup.Item>
             </ListGroup>
