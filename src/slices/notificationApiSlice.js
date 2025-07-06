87ee19a0e8b2810efdba19192df18fb482ca670f
kevyamon <gamekevy@gmail.com>
2025-07-06 21:42:07 +0000

    Ygg

--- src/slices/notificationApiSlice.js
+++ src/slices/notificationApiSlice.js
@@ -1,21 +1,27 @@
 import { apiSlice } from './apiSlice';
+const NOTIFICATION_URL = '/api/notifications';
 
 export const notificationApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getNotifications: builder.query({
-      query: () => '/api/notifications',
+      query: () => ({
+        url: `${NOTIFICATION_URL}`,
+        method: 'GET',
+      }),
       providesTags: ['Notifications'],
     }),
-    markAsRead: builder.mutation({
+
+    markAllAsRead: builder.mutation({
       query: () => ({
-        url: '/api/notifications/mark-as-read',
+        url: `${NOTIFICATION_URL}/read-all`,
         method: 'PUT',
       }),
       invalidatesTags: ['Notifications'],
     }),
+
     deleteNotification: builder.mutation({
       query: (notificationId) => ({
-        url: `/api/notifications/${notificationId}`,
+        url: `${NOTIFICATION_URL}/${notificationId}`,
         method: 'DELETE',
       }),
       invalidatesTags: ['Notifications'],
@@ -25,6 +31,6 @@ export const notificationApiSlice = apiSlice.injectEndpoints({
 
 export const {
   useGetNotificationsQuery,
-  useMarkAsReadMutation,
+  useMarkAllAsReadMutation,
   useDeleteNotificationMutation,
 } = notificationApiSlice;
