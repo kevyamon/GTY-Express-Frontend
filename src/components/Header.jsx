76788f4e97b2c434c6467862d9c46904219a9c31
kevyamon <gamekevy@gmail.com>
2025-07-06 10:54:01 +0000

    Ihjj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import { Navbar, Nav, Container, NavDropdown, Badge, Form, Button } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useState, useEffect, useMemo } from 'react';
-
+import { useState, useMemo } from 'react';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { useGetOrdersQuery, useGetMyOrdersQuery } from '../slices/orderApiSlice';
 import { useMarkAsReadMutation, useGetNotificationsQuery } from '../slices/notificationApiSlice';
@@ -18,19 +17,22 @@ const Header = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const { cartItems } = useSelector((state) => state.cart);
 
+  // --- AMÉLIORATION : Polling toutes les 5 secondes ---
+  const POLLING_INTERVAL = 5000;
+
   const { data: adminOrders } = useGetOrdersQuery(undefined, {
     skip: !userInfo?.isAdmin,
-    pollingInterval: 15000,
+    pollingInterval: POLLING_INTERVAL,
   });
 
   const { data: clientOrders } = useGetMyOrdersQuery(undefined, {
     skip: !userInfo || userInfo.isAdmin,
-    pollingInterval: 15000,
+    pollingInterval: POLLING_INTERVAL,
   });
 
   const { data: notifications } = useGetNotificationsQuery(undefined, {
     skip: !userInfo,
-    pollingInterval: 15000,
+    pollingInterval: POLLING_INTERVAL,
   });
 
   const [markAsRead] = useMarkAsReadMutation();
@@ -116,13 +118,23 @@ const Header = () => {
                     </Badge>
                   )}
                 </Nav.Link>
+                {/* On ajoute le lien vers la cloche de notification */}
+                {userInfo && (
+                  <Nav.Link as={Link} to="/notifications" onClick={handleNotificationClick}>
+                    🔔
+                    {unreadNotifsCount > 0 && (
+                      <Badge pill bg="danger" style={{ position: 'relative', top: '-10px', left: '-5px' }}>
+                        {unreadNotifsCount}
+                      </Badge>
+                    )}
+                  </Nav.Link>
+                )}
 
                 {userInfo ? (
                   <NavDropdown
                     title={
                       <div className='profile-icon-container'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                          className="bi bi-person-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                         </svg>
                       </div>
@@ -177,24 +189,12 @@ const Header = () => {
           />
           <Button type='submit' variant='outline-success' className='p-2 ms-2'>OK</Button>
         </Form>
-
         <div className="d-flex align-items-center mt-3">
-          {userInfo && (
-            <Link to="/notifications" onClick={handleNotificationClick} className="me-4">
-              🔔
-              {unreadNotifsCount > 0 && (
-                <Badge pill bg="danger">{unreadNotifsCount}</Badge>
-              )}
-            </Link>
-          )}
-
           <Link to="/products" className="home-icon-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
-              className="bi bi-house-door-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
               <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
             </svg>
           </Link>
-
           <Link to="/supermarket" className="supermarket-btn ms-4">
             🛍️<span className="ms-2 d-none d-lg-block">Supermarché</span>
           </Link>
