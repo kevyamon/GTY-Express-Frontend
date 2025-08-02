cb8cf994842dfe1a5a2ff3adf9a20fe7efce1fc4
kevyamon <gamekevy@gmail.com>
2025-08-02 15:29:57 +0000

    bugs Filtre

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom'; // IMPORTATION DE useLocation AJOUTÉE
 import Product from '../components/Product';
 import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
@@ -7,6 +7,7 @@ import './HomeScreen.css';
 
 const HomeScreen = () => {
   const { keyword, category: categoryFromUrl } = useParams();
+  const location = useLocation(); // DÉCLARATION DE location AJOUTÉE
 
   const isSupermarket = location.pathname.startsWith('/supermarket');
   const isPromo = location.pathname.startsWith('/promotions');
@@ -44,11 +45,11 @@ const HomeScreen = () => {
       : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) 
       : (
         <>
-          {products.length === 0 ? (
+          {products && products.length === 0 ? (
             <Message>Aucun produit trouvé pour cette catégorie.</Message>
           ) : (
             <Row>
-              {products.map((product) => (
+              {products && products.map((product) => (
                 <Col key={product._id} xs={6} md={6} lg={4} xl={3}>
                   <Product product={product} />
                 </Col>
