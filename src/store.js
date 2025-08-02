3389f005ed6cb5f047d32ed2766456b668d3d59c
kevyamon <gamekevy@gmail.com>
2025-08-02 10:53:38 +0000

    2

--- src/store.js
+++ src/store.js
@@ -1,23 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
-// On corrige le chemin ici pour pointer vers le dossier 'slices'
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
 import authReducer from './slices/authSlice';
 import favoritesReducer from './slices/favoritesSlice';
-import { promotionApiSlice } from './slices/promotionApiSlice';
-import { notificationApiSlice } from './slices/notificationApiSlice';
 
 const store = configureStore({
   reducer: {
+    // Seul le reducer de la slice d'API principale est nécessaire
     [apiSlice.reducerPath]: apiSlice.reducer,
-    [promotionApiSlice.reducerPath]: promotionApiSlice.reducer,
-    [notificationApiSlice.reducerPath]: notificationApiSlice.reducer,
     cart: cartSliceReducer,
     auth: authReducer,
     favorites: favoritesReducer,
   },
+  // Seul le middleware de la slice d'API principale est nécessaire
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware, promotionApiSlice.middleware, notificationApiSlice.middleware),
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
