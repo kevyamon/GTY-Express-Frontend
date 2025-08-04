52150f0d46e0b93b102f64b583f233b76ded85a9
kevyamon <gamekevy@gmail.com>
2025-08-04 10:42:34 +0000

    Bugs

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -23,44 +23,76 @@ export const apiSlice = createApi({
         arg,
         { updateCachedData, cacheDataLoaded, cacheEntryRemoved, dispatch, getState }
       ) {
-        const socket = io(import.meta.env.VITE_BACKEND_URL, {
-            query: { userId: getState().auth.userInfo?._id },
+        const socket = io(import.meta.env.VITE_BACKEND_URL);
+
+        // --- LOGIQUE DE CONNEXION CORRIGÉE ---
+        socket.on('connect', () => {
+          console.log('Socket.IO connecté !');
+          const { userInfo } = getState().auth;
+          if (userInfo) {
+            // L'utilisateur rejoint sa room personnelle
+            socket.emit('joinRoom', userInfo._id);
+            // Si c'est un admin, il rejoint aussi la room 'admin'
+            if (userInfo.isAdmin) {
+              socket.emit('joinRoom', 'admin');
+            }
+          }
         });
 
-        socket.on('connect', () => { console.log('Socket.IO connecté !'); });
-        socket.on('disconnect', () => { console.log('Socket.IO déconnecté.'); });
+        socket.on('disconnect', () => {
+          console.log('Socket.IO déconnecté.');
+        });
 
-        socket.on('order_update', (data) => { dispatch(apiSlice.util.invalidateTags(['Order'])); });
-        socket.on('notification', (data) => { dispatch(apiSlice.util.invalidateTags(['Notification'])); });
+        socket.on('order_update', (data) => {
+          console.log('Événement order_update reçu', data);
+          dispatch(apiSlice.util.invalidateTags(['Order']));
+        });
+
+        socket.on('notification', (data) => {
+          console.log('Événement notification reçu', data);
+          dispatch(apiSlice.util.invalidateTags(['Notification']));
+        });
+
         socket.on('product_update', (data) => {
+          console.log('Événement product_update reçu', data);
           dispatch(apiSlice.util.invalidateTags(['Product']));
           dispatch(apiSlice.util.invalidateTags([{ type: 'Product', id: data.productId }]));
         });
+
         socket.on('newMessage', (newMessage) => {
+            console.log('Nouveau message reçu en temps réel', newMessage);
             dispatch(apiSlice.util.invalidateTags(['Conversation']));
             dispatch(apiSlice.util.invalidateTags([{ type: 'Message', id: newMessage.conversationId }]));
         });
+
         socket.on('conversationRead', (data) => {
+            console.log('Conversation lue en temps réel', data);
             dispatch(apiSlice.util.invalidateTags(['Conversation']));
         });
+
         socket.on('messageDeleted', (data) => {
+            console.log('Message supprimé en temps réel', data);
             dispatch(apiSlice.util.invalidateTags(['Conversation']));
             dispatch(apiSlice.util.invalidateTags([{ type: 'Message', id: data.conversationId }]));
         });
+
         socket.on('messageEdited', (editedMessage) => {
+            console.log('Message modifié en temps réel', editedMessage);
             dispatch(apiSlice.util.invalidateTags([{ type: 'Message', id: editedMessage.conversationId }]));
         });
+
         socket.on('status_update', (data) => {
             dispatch(updateUserStatus({ _id: getState().auth.userInfo._id, ...data }));
         });
+
         socket.on('new_complaint', (data) => {
             dispatch(apiSlice.util.invalidateTags(['Complaint']));
         });
+
         socket.on('complaint_update', () => {
             dispatch(apiSlice.util.invalidateTags(['Complaint']));
         });
 
-        // NOUVEL ÉCOUTEUR
         socket.on('banner_update', () => {
             console.log('Mise à jour de la bannière reçue');
             dispatch(apiSlice.util.invalidateTags(['PromoBanner']));
